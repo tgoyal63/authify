cd324ff6518387a1e7c5aaeaa0cd7b5f323042d5
Sidhant <alairoet@gmail.com>
2023-10-29 23:10:55 +0530

    changed response data structure in sheet verification route

--- src/controllers/sheet.controller.ts
+++ src/controllers/sheet.controller.ts
@@ -1,5 +1,5 @@
-import { Response } from "express";
-import { getInternalSheets } from "../utils/sheet.utils";
+import { Request, Response } from "express";
+import { getInternalSheets, getSheetData } from "../utils/sheet.utils";
 import { TypedRequestQuery } from "zod-express-middleware";
 import {
 	getInternalSheetValidator,
@@ -23,11 +23,35 @@ export const getInternalSheetController = async (
 			res.send({ success: false, message: "No sheet found" });
 			return;
 		}
+
+		const respponseData = internalSheetData.map((sheet) => {
+			return {
+				id: sheet.properties?.sheetId,
+				title: sheet.properties?.title,
+				index: sheet.properties?.index,
+			};
+		});
 		res.send({
 			success: true,
-			data: internalSheetData,
+			data: respponseData,
 			message: "Internal sheet data fetched succesfully.",
 		});
+	} catch (error: any) {
+		res.status(500).send({ message: error.message, success: false });
+	}
+};
+
+export const getSheetDataController = async (req: Request, res: Response) => {
+	try {
+		const shes = req.query?.["sheetUrl"] as string;
+		const sheetSplit = shes.match(sheetRegex);
+		if (!sheetSplit) {
+			res.send({ success: false, message: "No sheet found" });
+			return;
+		}
+		const sheetId = sheetSplit[1] as string;
+		const data = getSheetData(sheetId, "sid");
+		res.send({ success: true, data, message: "Data fetched successfully" });
 	} catch (error) {
 		console.log(error);
 	}
