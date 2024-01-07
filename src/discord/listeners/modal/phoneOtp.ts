60e214a2c83893ca9c3245910a942a7f31e3d72d
Sidhant <alairoet@gmail.com>
2024-01-07 16:38:26 +0530

    otp validator itnegration

--- src/discord/listeners/modal/phoneOtp.ts
+++ src/discord/listeners/modal/phoneOtp.ts
@@ -4,65 +4,109 @@ import {
     ButtonStyle,
     ModalSubmitInteraction,
 } from "discord.js";
-import { getColumnDataofService } from "../../../services/service.service";
+import {
+    getColumnDataofService,
+    getServiceData,
+} from "../../../services/service.service";
 
 import { generateOtpForDiscordId, sendOtp } from "../../../utils/otp.utils";
+import gangstaPhilosophy from "@/customSolutions/gangstaPhilosophy";
 
 export default async (
     interaction: ModalSubmitInteraction,
     serviceId: string,
 ) => {
     try {
+        const service = await getServiceData(serviceId);
         const phone = interaction.fields.getTextInputValue("phone");
-        const { phoneNumbers, discordIds } =
-            await getColumnDataofService(serviceId);
-        const existing = discordIds.find((id, i) => {
-            if (phoneNumbers[i] === phone) {
-                return false;
+        if (service?.integrationType === "tagMango") {
+            const tmCheck = await gangstaPhilosophy.subscriberValidator(
+                serviceId,
+                +phone,
+                interaction.user.id,
+            );
+            if (tmCheck) {
+                const otp = generateOtpForDiscordId(interaction.user.id);
+                await sendOtp(parseInt(phone), otp);
+
+                const verifyButton = new ButtonBuilder()
+                    .setCustomId(
+                        `authifyButton-verifyPhone-${phone}-${serviceId}`,
+                    )
+                    .setLabel("Verify OTP")
+                    .setStyle(ButtonStyle.Success);
+
+                const actionRow =
+                    new ActionRowBuilder<ButtonBuilder>().addComponents(
+                        verifyButton,
+                    );
+
+                await interaction.reply({
+                    content: "An OTP has been sent to your phone number.",
+                    ephemeral: true,
+                    components: [actionRow],
+                });
+                return;
             }
-            return id === interaction.user.id;
-        });
-        if (existing) {
             await interaction.reply({
-                content: "You have already registered.",
+                content: "We couldnt verify your phone number.Please try again or reach out to the support channel.",
                 ephemeral: true,
             });
-            return;
-        }
-        if (!phoneNumbers.includes(phone)) {
-            await interaction.reply({
-                content:
-                    "Phone number not found in course. Please contact the course admin.",
-                ephemeral: true,
+        } else {
+            const { phoneNumbers, discordIds } =
+                await getColumnDataofService(serviceId);
+            const existing = discordIds.find((id, i) => {
+                if (phoneNumbers[i] === phone) {
+                    return false;
+                }
+                return id === interaction.user.id;
             });
-            return;
-        }
+            if (existing) {
+                await interaction.reply({
+                    content: "You have already registered.",
+                    ephemeral: true,
+                });
+                return;
+            }
+            if (!phoneNumbers.includes(phone)) {
+                await interaction.reply({
+                    content:
+                        "Phone number not found in course. Please contact the course admin.",
+                    ephemeral: true,
+                });
+                return;
+            }
 
-        const existingDiscordId = discordIds[phoneNumbers.indexOf(phone)];
-        if (existingDiscordId && existingDiscordId !== interaction.user.id) {
+            const existingDiscordId = discordIds[phoneNumbers.indexOf(phone)];
+            if (
+                existingDiscordId &&
+                existingDiscordId !== interaction.user.id
+            ) {
+                await interaction.reply({
+                    content: `Phone number already registered with <@${existingDiscordId}>.`,
+                    ephemeral: true,
+                });
+                return;
+            }
+            const otp = generateOtpForDiscordId(interaction.user.id);
+            await sendOtp(parseInt(phone), otp);
+
+            const verifyButton = new ButtonBuilder()
+                .setCustomId(`authifyButton-verifyPhone-${phone}-${serviceId}`)
+                .setLabel("Verify OTP")
+                .setStyle(ButtonStyle.Success);
+
+            const actionRow =
+                new ActionRowBuilder<ButtonBuilder>().addComponents(
+                    verifyButton,
+                );
+
             await interaction.reply({
-                content: `Phone number already registered with <@${existingDiscordId}>.`,
+                content: "An OTP has been sent to your phone number.",
                 ephemeral: true,
+                components: [actionRow],
             });
-            return;
         }
-        const otp = generateOtpForDiscordId(interaction.user.id);
-        await sendOtp(parseInt(phone), otp);
-
-        const verifyButton = new ButtonBuilder()
-            .setCustomId(`authifyButton-verifyPhone-${phone}-${serviceId}`)
-            .setLabel("Verify OTP")
-            .setStyle(ButtonStyle.Success);
-
-        const actionRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
-            verifyButton,
-        );
-
-        await interaction.reply({
-            content: "An OTP has been sent to your phone number.",
-            ephemeral: true,
-            components: [actionRow],
-        });
     } catch (error: any) {
         console.log(error);
         if (error.message === "Fast2SMS API error.") {
